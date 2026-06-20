d401c4f179727df258ed235b0e1f29bff0f7b086
Ozlek <kelzolumabaskelzo@email.com>
2026-06-21 01:56:52 +0800

    Updated settings.py for fixes

--- backend/equinoxSite/settings.py
+++ backend/equinoxSite/settings.py
@@ -82,6 +82,8 @@ SESSION_COOKIE_SECURE = os.getenv('SESSION_COOKIE_SECURE', 'False') == 'True'
 CSRF_COOKIE_SAMESITE = 'None'
 CSRF_COOKIE_SECURE = os.getenv('CSRF_COOKIE_SECURE', 'False') == 'True'
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
